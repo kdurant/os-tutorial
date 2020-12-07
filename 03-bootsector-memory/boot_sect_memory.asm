90b3a26f0454a6169dda9133e4f7743c3ccd58bb
kdurant <wangjun850725@163.com>
2020-12-07 21:44:34 +0800

    feat: add picture in 03-dir

--- 03-bootsector-memory/boot_sect_memory.asm
+++ 03-bootsector-memory/boot_sect_memory.asm
@@ -33,10 +33,10 @@ int 0x10
 ; We try a shortcut since we know that the X is stored at byte 0x2d in our binary
 ; That's smart but ineffective, we don't want to be recounting label offsets
 ; every time we change the code
-mov al, "4"
-int 0x10
-mov al, [0x7c2d]
-int 0x10
+;mov al, "4"
+;int 0x10
+;mov al, [0x7c2d]
+;int 0x10
 
 
 jmp $ ; infinite loop
